0d7686d1065bfd9a2c5bc34f131e5530486a0d66
bryceandy <bryceandy@rocketmail.com>
2020-12-31 14:21:24 +0300

    Create the top navigation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Video Games</title>
+
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="{{ asset('laracasts-logo.svg') }}" alt="" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input
+                        type="text"
+                        class="bg-gray-800 text-sm rounded-full px-3 py-1 w-64 focus:outline-none pl-8"
+                        placeholder="Search..."
+                    >
+                    <div class="absolute top-0 flex items-center ml-2 h-full">
+                        <a href="#">
+                            <svg
+                                class="text-gray-400 w-4 fill-current"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path d="M456.69 421.39L362.6 327.3a173.81 173.81 0 0034.84-104.58C397.44 126.38 319.06 48 222.72 48S48 126.38 48 222.72s78.38 174.72 174.72 174.72A173.81 173.81 0 00327.3 362.6l94.09 94.09a25 25 0 0035.3-35.3zM97.92 222.72a124.8 124.8 0 11124.8 124.8 124.95 124.95 0 01-124.8-124.8z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#">
+                        <img src="{{ asset('larydefault.svg') }}" alt="avatar" class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    @yield('content')
+</body>
+</html>
