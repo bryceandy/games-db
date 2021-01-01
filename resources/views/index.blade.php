e5536ccad34836260c2e512f85e8a46d5066b555
bryceandy <bryceandy@rocketmail.com>
2021-01-01 15:00:10 +0300

    Add responsiveness to the index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -240,8 +240,8 @@
             </div>
         </div>
         {{--End popular games--}}
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
@@ -262,7 +262,7 @@
                                 Demon's Souls
                             </a>
                             <div class="text-gray-400 mt-1">Playstation 5</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden md:block">
                                 A full remake of Demon's Souls (2009) featuring improved graphics and animations, sound and lighting tweaks and a reimagining of many of the visual, musical and mechanical aspects of the original game.
                             </p>
                         </div>
@@ -287,7 +287,7 @@
                             <div class="text-gray-400 mt-1">
                                 Xbox One, PlayStation 4, PlayStation 5, PC (Microsoft Windows), Google Stadia, Xbox Series
                             </div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden md:block">
                                 In Watch Dogs: Legion, near future London is facing its downfall...unless you do something about it. Build a resistance, fight back, and give the city back to the people. It’s time to rise up.
                             </p>
                         </div>
@@ -312,7 +312,7 @@
                             <div class="text-gray-400 mt-1">
                                 Xbox One, PlayStation 4, PlayStation 5, PC (Microsoft Windows), Nintendo Switch, Google Stadia, Xbox Series
                             </div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden md:block">
                                 From the creators of Assassin’s Creed Odyssey comes a storybook adventure about a forgotten hero on a quest to save the Greek gods.
 
                                 Embark on a journey to the Isle of the Blessed, taken over by dangerous creatures of mythology.
@@ -322,7 +322,7 @@
                 </div>
             </div>
             {{--End recently viewed--}}
-            <div class="most-anticipated-coming-soon w-1/4">
+            <div class="most-anticipated-coming-soon mt-12 lg:mt-0 w-1/4">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
                     <div class="game flex">
