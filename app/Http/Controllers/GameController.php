2265c6a211158fcac6288f8f482eaddb49d558c0
bryceandy <bryceandy@rocketmail.com>
2021-01-02 20:32:38 +0300

    Sort coming soon games

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -91,6 +91,7 @@ class GameController extends Controller
                     fields name, cover.url, first_release_date, rating_count, platforms.abbreviation, rating, slug;
                     where platforms = (48,49,130,6)
                         & (first_release_date >= ${current} & first_release_date < ${nextMonth});
+                    sort first_release_date asc;
                     limit 4;
                 ",
                 'text/plain'
