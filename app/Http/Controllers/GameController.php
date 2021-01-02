7ef5483e215374a8c81b205383bc30affbe2c10b
bryceandy <bryceandy@rocketmail.com>
2021-01-02 18:11:33 +0300

    Fetch games from API

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index(): Factory|View|Application
+    {
+        /*$authRequest = Http::post(
+            config('igdb.auth_url'),
+            [
+                'client_id' => config('igdb.credentials.client_id'),
+                'client_secret' => config('igdb.credentials.client_secret'),
+                'grant_type' => 'client_credentials',
+            ]
+        );*/
+        $games = Http::withHeaders([
+            'Client-ID' => config('igdb.credentials.client_id'),
+            'Authorization' => "Bearer dtzj0cl1wqhisdiz8z9glxfko0uu1p",
+        ])
+            ->withBody(
+                "
+                    fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, slug;
+                    where total_rating_count > 5;
+                    sort total_rating_count desc;
+                    limit 12;
+                ",
+                'text/plain'
+            )
+            ->post(config('igdb.base_url') . 'games')
+            ->json();
+
+        return view('index', compact('games'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
