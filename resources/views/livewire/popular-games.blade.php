778d67569a6f7177b47fe129431c99071f35d3a5
bryceandy <bryceandy@rocketmail.com>
2021-01-03 15:06:23 +0300

    Display a temporary loader

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -2,7 +2,7 @@
     wire:init="loadPopularGames"
     class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16"
 >
-    @foreach($popularGames as $game)
+    @forelse($popularGames as $game)
         <div class="game mt-8 flex md:block flex-col items-center">
             <div class="relative inline-block">
                 <a href="/games/{{ $game['slug'] }}">
@@ -28,5 +28,9 @@
                 {{ collect(collect($game['platforms'])->pluck('abbreviation'))->join(", ") }}
             </div>
         </div>
-    @endforeach
+        @empty
+        <div class="container pt-8 font-bold">
+            <p>Loading ...</p>
+        </div>
+    @endforelse
 </div>
