4770e4d80fe75af2f89bb1409d3b9eee05883f95
bryceandy <bryceandy@rocketmail.com>
2021-01-04 21:56:36 +0300

    Clean up

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -3,7 +3,7 @@
     class="coming-soon-container space-y-10 mt-8"
 >
     @forelse($comingSoon as $game)
-        <x-game-card-small :game="$game"/>
+        <x-game-card-small :game="$game" />
     @empty
         @foreach(range(1, 4) as $game)
             <div class="game flex animate-pulse">
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -3,7 +3,7 @@
     class="most-anticipated-container space-y-10 mt-8"
 >
     @forelse($mostAnticipated as $game)
-        <x-game-card-small :game="$game"/>
+        <x-game-card-small :game="$game" />
         @empty
             @foreach(range(1, 4) as $game)
                 <div class="game flex animate-pulse">
