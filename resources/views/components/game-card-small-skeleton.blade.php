c8469cc432204ce701f1674cc425ead8878bc6ca
bryceandy <bryceandy@rocketmail.com>
2021-01-04 22:29:00 +0300

    Create anon component for skleton loaders

---
+++ resources/views/components/game-card-small-skeleton.blade.php
@@ -0,0 +1,7 @@
+<div class="game flex animate-pulse">
+    <div class="bg-gray-800 w-20 h-20 rounded-xl"></div>
+    <div class="ml-4">
+        <div class="block h-4 bg-gray-700 text-transparent rounded-xl">Title goes here</div>
+        <div class="block mt-1 h-4 w-3/4 bg-gray-700 text-transparent rounded-xl">21 Jan, 2020</div>
+    </div>
+</div>
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -6,13 +6,7 @@
         <x-game-card-small :game="$game" />
         @empty
             @foreach(range(1, 4) as $game)
-                <div class="game flex animate-pulse">
-                    <div class="bg-gray-800 w-20 h-20 rounded-xl"></div>
-                    <div class="ml-4">
-                        <div class="block h-4 bg-gray-700 text-transparent rounded-xl">Title goes here</div>
-                        <div class="block mt-1 h-4 w-3/4 bg-gray-700 text-transparent rounded-xl">21 Jan, 2020</div>
-                    </div>
-                </div>
+                <x-game-card-small-skeleton />
             @endforeach
     @endforelse
 </div>
