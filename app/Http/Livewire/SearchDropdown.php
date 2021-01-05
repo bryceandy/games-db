50afcf38ddbdc9e3ee5bbbaa326c480b3ed71c81
bryceandy <bryceandy@rocketmail.com>
2021-01-05 23:43:41 +0300

    Search and render

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,51 @@
 
 namespace App\Http\Livewire;
 
+use App\Traits\FormatsGames;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
-    public function render()
+    use FormatsGames;
+
+    public string $search = '';
+
+    public array $searchResults = [];
+
+    public function render(): Factory|View|Application
     {
+        $response = Http::withHeaders([
+            'Client-ID' => config('igdb.credentials.client_id'),
+            'Authorization' => "Bearer dtzj0cl1wqhisdiz8z9glxfko0uu1p",
+        ])
+            ->withBody(
+                "
+                    search \"{$this->search}\";
+                    fields name, cover.url, slug;
+                    limit 6;
+                ",
+                'text/plain'
+            )
+            ->post(config('igdb.base_url') . 'games');
+
+        $this->handleResponse($response);
+
         return view('livewire.search-dropdown');
+    }
+
+    private function handleResponse(Response $response)
+    {
+        if ($response->successful()) {
+            $this->searchResults = collect($response->json())
+                ->map(fn ($game) => collect($game)->merge([
+                    'cover_url' => $this->getImageUrl($game,'micro', 'cover'),
+                ]))
+                ->toArray();
+        }
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,6 +1,7 @@
 <div class="relative">
     <label for="search"></label>
     <input
+        wire:model.debounce.300ms="search"
         type="text"
         id="search"
         class="bg-gray-800 text-sm rounded-full px-3 py-1 w-64 focus:outline-none focus:ring focus:border-blue-300 pl-8"
@@ -15,17 +16,21 @@
             d="M456.69 421.39L362.6 327.3a173.81 173.81 0 0034.84-104.58C397.44 126.38 319.06 48 222.72 48S48 126.38 48 222.72s78.38 174.72 174.72 174.72A173.81 173.81 0 00327.3 362.6l94.09 94.09a25 25 0 0035.3-35.3zM97.92 222.72a124.8 124.8 0 11124.8 124.8 124.95 124.95 0 01-124.8-124.8z"
         />
     </svg>
-    <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
-        <ul>
-            <li class="border-b border-gray-700">
-                <a
-                    href="#"
-                    class="rounded block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
-                >
-                    <img src="http://images.igdb.com/igdb/image/upload/t_micro/co1k9j.jpg" alt="cover" class="rounded">
-                    <span class="ml-4">Marvel's Spider-Man: Miles Morales</span>
-                </a>
-            </li>
-        </ul>
-    </div>
+    @if(count($searchResults))
+        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+            <ul>
+                @foreach($searchResults as $game)
+                    <li class="border-b border-gray-700">
+                        <a
+                            href="{{ route('games.show', $game['slug']) }}"
+                            class="rounded block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                        >
+                            <img src="{{ $game['cover_url'] }}" alt="cover" class="rounded">
+                            <span class="ml-4">{{ $game['name'] }}</span>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
