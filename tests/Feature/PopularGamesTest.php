3a3de3ac3dd43ecc4e979383925b96cfb02c7e3c
bryceandy <bryceandy@rocketmail.com>
2021-01-05 09:20:02 +0300

    Test popular games component

--- tests/Feature/PopularGamesTest.php
+++ tests/Feature/PopularGamesTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\PopularGames;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class PopularGamesTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    /** @test */
     public function the_home_page_shows_popular_games()
     {
-        
+        Livewire::test(PopularGames::class)
+            ->assertSet('popularGames', [])
+            ->call('loadGames')
+            ->assertSee("Marvel's Spider-Man: Miles Morales");
     }
 }
