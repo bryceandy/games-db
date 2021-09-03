d6166adff6da5ec5607cd4b2938326e99f80beff
bryceandy <bryceandy@rocketmail.com>
2021-09-03 16:29:16 +0300

    Cache twitch auth token

---
+++ app/Traits/Auth/RetrievesAuthHeaders.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits\Auth;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use JetBrains\PhpStorm\ArrayShape;
+
+trait RetrievesAuthHeaders
+{
+    #[ArrayShape([
+        'Client-ID' => "\Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|mixed",
+        'Authorization' => "string",
+    ])]
+    public function fetchIgdbHeaders(): array
+    {
+        $token = Cache::remember(
+            'twitch_access_token',
+            4804500,
+            fn() => Http::post(
+                config('igdb.auth_url'),
+                [
+                    'client_id' => config('igdb.credentials.client_id'),
+                    'client_secret' => config('igdb.credentials.client_secret'),
+                    'grant_type' => 'client_credentials',
+                ]
+            )->json('access_token')
+        );
+
+        return [
+            'Client-ID' => config('igdb.credentials.client_id'),
+            'Authorization' => "Bearer $token",
+        ];
+    }
+}
