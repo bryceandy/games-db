b8af8703b7c24da2a5cefade6016076502a8cbb4
bryceandy <bryceandy@rocketmail.com>
2021-09-03 21:34:57 +0300

    Cache reviewed games

--- app/Http/Livewire/RecentlyReviewed.php
+++ app/Http/Livewire/RecentlyReviewed.php
@@ -7,7 +7,7 @@ use App\Traits\FormatsGames;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -25,35 +25,37 @@ class RecentlyReviewed extends Component
 
     public function loadGames()
     {
-        $before = now()->subMonths(2)->timestamp;
-        $current = now()->timestamp;
+        $response = Cache::remember('reviewed_games', now(), function () {
+            $before = now()->subMonths(2)->timestamp;
+            $current = now()->timestamp;
 
-        $response = Http::withHeaders($this->fetchIgdbHeaders())
-            ->withBody(
-                "
-                    fields name, cover.url, first_release_date, rating_count, platforms.abbreviation, rating, summary, slug, storyline;
+            return Http::withHeaders($this->fetchIgdbHeaders())
+                ->withBody(
+                    "
+                    fields name, cover.url, first_release_date, rating_count, platforms.abbreviation, rating, summary,
+                    slug, storyline;
                     where platforms = (48,49,130,6)
                         & rating_count > 5
                         & (first_release_date >= ${before} & first_release_date < ${current});
                     limit 3;
                 ",
-                'text/plain'
-            )
-            ->post(config('igdb.base_url') . 'games');
+                    'text/plain'
+                )
+                ->post(config('igdb.base_url') . 'games')
+                ->json();
+        });
 
         $this->handleResponse($response);
     }
 
-    private function handleResponse(Response $response)
+    private function handleResponse($response)
     {
-        if ($response->successful()) {
-            $this->recentlyReviewed = $this->bigCoverGames($response->json());
+        $this->recentlyReviewed = $this->bigCoverGames($response);
 
-            collect($this->recentlyReviewed)
-                ->each(fn ($game) => $this->emit('reviewGameWithRatingAdded', [
-                    'id' => 'review_' . $game['slug'],
-                    'rating' => $game['rating'],
-                ]));
-        }
+        collect($this->recentlyReviewed)
+            ->each(fn ($game) => $this->emit('reviewGameWithRatingAdded', [
+                'id' => 'review_' . $game['slug'],
+                'rating' => $game['rating'],
+            ]));
     }
 }
