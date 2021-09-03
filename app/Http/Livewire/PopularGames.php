de6749f9d9245df29d7576339e6d9ba1eb78824b
bryceandy <bryceandy@rocketmail.com>
2021-09-03 21:30:40 +0300

    Cache popular games

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -7,7 +7,7 @@ use App\Traits\FormatsGames;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -25,12 +25,13 @@ class PopularGames extends Component
 
     public function loadGames(): void
     {
-        $before = now()->subMonths(2)->timestamp;
-        $after = now()->addMonths(2)->timestamp;
+        $response = Cache::remember('popular_games', now()->addDays(3), function () {
+            $before = now()->subMonths(2)->timestamp;
+            $after = now()->addMonths(2)->timestamp;
 
-        $response = Http::withHeaders($this->fetchIgdbHeaders())
-            ->withBody(
-                "
+            return Http::withHeaders($this->fetchIgdbHeaders())
+                ->withBody(
+                    "
                     fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, slug;
                     where platforms = (48,49,130,6)
                         & total_rating_count > 5
@@ -38,24 +39,24 @@ class PopularGames extends Component
                     sort total_rating_count desc;
                     limit 12;
                 ",
-                'text/plain'
-            )
-            ->post(config('igdb.base_url') . 'games');
+                    'text/plain'
+                )
+                ->post(config('igdb.base_url') . 'games')
+                ->json();
+        });
 
         $this->handleResponse($response);
     }
 
-    private function handleResponse(Response $response)
+    private function handleResponse($response)
     {
-        if ($response->successful()) {
-            $this->popularGames = $this->bigCoverGames($response->json());
+        $this->popularGames = $this->bigCoverGames($response);
 
-            collect($this->popularGames)
-                ->filter(fn ($game) => $game['rating'])
-                ->each(fn ($game) => $this->emit('gameWithRatingAdded', [
-                    'id' => $game['slug'],
-                    'rating' => $game['rating'],
-                ]));
-        }
+        collect($this->popularGames)
+            ->filter(fn ($game) => $game['rating'])
+            ->each(fn ($game) => $this->emit('gameWithRatingAdded', [
+                'id' => $game['slug'],
+                'rating' => $game['rating'],
+            ]));
     }
 }
